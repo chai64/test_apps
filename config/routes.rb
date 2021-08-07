a28096c3ab3224f8992fb02a1f8f64df8024471f
Chaitanya Nadkarny <chaitanya@ragya.com>
2021-08-07 19:29:04 +0530

    Added README.md

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # the route name is the same as teh application_controller.rb in the Controllers dir
+  root 'pages#home'
+end
