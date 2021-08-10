5ee4e203ceaa703fea39212e3148c7a1b00a6e6e
Chaitanya Nadkarny <chaitanya@ragya.com>
2021-08-10 19:47:24 +0530

    Added Gharana and About pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # the route name is the same as teh application_controller.rb in the Controllers dir
   root 'pages#home'
+  get 'about', to: 'pages#about'
 end
